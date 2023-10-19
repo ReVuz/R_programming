4ae0a7a5353221095ca12834d30e25b072ce66a6
ReVuz <revathymr916@gmail.com>
2023-10-19 12:15:05 +0530

    Fixed Errors

--- q7.R
+++ q7.R
@@ -3,29 +3,22 @@
 # last three terms.
 
 
-generate_series <- function(n) {
-  series <- numeric(n)
+# Function to print the Fibonacci sequence using a loop
+print_fibonacci <- function(n) {
+  a <- 0
+  b <- 1
+  c <- 1
   
-#  Initialize the first three terms
-  series[1] <- 0
-  series[2] <- 1
-  series[3] <- 2
-  
-#  Generate subsequent terms
-  for (i in 4:n) {
-    series[i] <- series[i - 1] + series[i - 2] + series[i - 3]
+  cat("Fibonacci Sequence:")
+  for (i in 1:n) {
+    cat(a, " ")
+    next_num <- a + b + c
+    a <- b
+    b <- c
+    c <- next_num
   }
-  
-  return(series)
 }
 
 #Input the number of terms from the user
-n <- as.integer(readline("Enter the number of terms in the series: "))
-
-if (n < 1) {
-  cat("Please enter a valid number of terms greater than or equal to 1.\n")
-} else {
-  series <- generate_series(n)
-  cat("The series with", n, "terms is:\n")
-  cat(series, "\n")
-}
+number_of_terms <- as.integer(readline("Enter the number of terms in the series: "))
+print_fibonacci(number_of_terms)
